d6b8656927d6f6f9e99a167dbc63eb541bdc055b
Anthony-Ricse <dokouser98@gmail.com>
2022-01-31 23:39:45 -0300

    server

---
+++ data.js
@@ -0,0 +1,60 @@
+const data = {
+  productos: [
+    {
+      id: 1,
+      nombre: "FIFA 22",
+      slug: "FIFA 22",
+      etiqueta: "Futbol",
+      categoria: "Deporte",
+      image: "/images/Fifa22.jpg",
+      precio: 45,
+      stock: 10,
+      rating: 4.5,
+      numVista: 10,
+      descripcion: "Es el mejor juego de futbol para compartir entre todos ",
+    },
+    {
+      id: 2,
+      nombre: "Grand The Auto V",
+      slug: "Grand The Auto V",
+      etiqueta: "Action",
+      categoria: "Mundo Abierto",
+      image: "/images/gtav.jpg",
+      precio: 60,
+      stock: 14,
+      rating: 4.5,
+      numVista: 10,
+      descripcion:
+        "Juego de mundo abierto , donde puedes disfrutar de nuevos retos junto a tus amigos",
+    },
+    {
+      id: 3,
+      nombre: "Spiderman",
+      slug: "Spiderman",
+      etiqueta: "Action",
+      categoria: "Mundo Abierto",
+      image: "/images/spiderman.png",
+      precio: 55,
+      stock: 14,
+      rating: 4.5,
+      numVista: 10,
+      descripcion:
+        "Un juego de mundo abierto donde exploras el mundo de Peter Parker y Max Morales",
+    },
+    {
+      id: 4,
+      nombre: "God Of War IV",
+      slug: "God Of War IV",
+      etiqueta: "Action",
+      categoria: "Mundo Abierto",
+      image: "/images/GodOfWar.jpg",
+      precio: 60,
+      stock: 14,
+      rating: 4.5,
+      numVista: 10,
+      descripcion:
+        "En un mundo nordico , Kratos ira descubriendo el nuevo mundo y afrontar los nuevos retos ademas de hacer frente a su pasado",
+    },
+  ],
+};
+export default data;
---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import data from "./data.js";
+//routes
+const app = express();
+
+//midleware
+app.get("/api/productos", (req, res) => {
+  res.send(data);
+});
+
+// puertos del servidor
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`servidor hosteado en http://localhost:${port}`);
+});
