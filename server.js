2afe0e59c3965c283247a2c5a935423f248ec5c0
Anthony-Ricse <dokouser98@gmail.com>
2022-02-06 18:54:13 -0300

    correciones

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const app = express();
 app.get("/api/productos", (req, res) => {
   res.send(data);
 });
+
+app.get("/producto/api/productos/slug/:slug", (req, res) => {
+  const producto = data.productos.find((x) => x.slug === req.params.slug);
+  console.log(producto);
+
+  if (producto) {
+    res.send(producto);
+  } else {
+    res.status(404).send({ message: "Producto no encontrado" });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("server operando");
 });
